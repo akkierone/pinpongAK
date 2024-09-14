9eb823daaee6877540bd15ccc7968964226a7f69
akkierone <pogu_ri_nui@ezweb.ne.jp>
2024-09-14 13:22:18 +0900

    Delete js/ui.js

--- js/ui.js
+++
@@ -1,5 +0,0 @@
-export function handleTouchMove(e) {
-    e.preventDefault();
-    const rect = document.getElementById('gameCanvas').getBoundingClientRect();
-    touchX = e.touches[0].clientX - rect.left;
-}
