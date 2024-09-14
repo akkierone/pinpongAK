193b33955758adfe4e0e9ee246493c8faa681b33
akkierone <pogu_ri_nui@ezweb.ne.jp>
2024-09-14 13:23:09 +0900

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,3 @@
+// ユーザーインターフェース関連のロジック
+document.getElementById('startButton').addEventListener('click', startCountdown);
+document.getElementById('restartButton').addEventListener('click', restartGame);
